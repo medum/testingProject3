7d259950ad04892f6d7aefd78eb042d5995f88fc
medum <49653011+medum@users.noreply.github.com>
2021-05-26 12:37:53 -0400

    Update vm.tf

--- Terraform/modules/vm/vm.tf
+++ Terraform/modules/vm/vm.tf
@@ -20,10 +20,10 @@ resource "azurerm_linux_virtual_machine" "test" {
   source_image_id       = var.packer_image
   disable_password_authentication = true
 
-  network_interface_ids = [azurerm_network_interface.test.id]
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file(var.public_key_path)
+#   network_interface_ids = [azurerm_network_interface.test.id]
+#   admin_ssh_key {
+#     username   = var.admin_username
+#     public_key = file(var.public_key_path)
   }
   os_disk {
     caching              = "ReadWrite"
