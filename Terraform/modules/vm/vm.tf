b94d67fd102ea858d7dabc1e6a094196d1505849
medum <49653011+medum@users.noreply.github.com>
2021-05-28 22:49:15 -0400

    Update vm.tf

--- Terraform/modules/vm/vm.tf
+++ Terraform/modules/vm/vm.tf
@@ -1,6 +1,6 @@
   
 provider "azurerm" {
-  version = ">= 2.0.0"
+  
   features {}
   subscription_id = "d869cbf8-8990-4e0b-94ca-9ec4e5ee2463"
   client_id       = "2e4a533b-0e77-45f3-a33b-bcde627341f6"
@@ -14,24 +14,24 @@ data "azurerm_image" "packer-image" {
 
   resource "azurerm_network_interface" "test" {
   name                = "udacity-project-3-NIC"
-  location            = var.location
-  resource_group_name = var.resource_group
+  location            = "East US"
+  resource_group_name = "proj3-packerIMG"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = var.subnet_id
+    subnet_id                     = "demo-rg-vnet/default"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = var.public_ip
+    public_ip_address_id          = "13.90.248.92"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                  = var.name
-  location              = var.location
-  resource_group_name   = var.resource_group
+  name                  = "demo-vm"
+  location              = "East US"
+  resource_group_name   = "proj3-packerIMG"
   size                  = "Standard_B2s"
-  admin_username        = var.admin_username
-  source_image_id       = var.packer_image
+  admin_username        = "admin"
+  source_image_id       = "myPackerImage"
   disable_password_authentication = true
 
   network_interface_ids = [azurerm_network_interface.test.id]
