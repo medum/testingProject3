8a2653cfb32208b673fcef02467734221c3021c3
medum <49653011+medum@users.noreply.github.com>
2021-05-29 21:20:38 -0400

    Update network.tf

--- Terraform/modules/network/network.tf
+++ Terraform/modules/network/network.tf
@@ -5,7 +5,7 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name  = var.resource_group
 }
 resource "azurerm_subnet" "test" {
-  name                 = "test-ntwrk-sub-1"
+  name                 = "test-ntwrk-sub"
   resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes       = ["10.0.1.0/24"]
